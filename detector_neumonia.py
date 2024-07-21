a827edebfc93374ac5d002314a36a39664613340
Miguel Sanabria <miguel_ang.sanabria@uao.edu.co>
2024-07-21 00:23:39 -0500

    Fix Image Resampler method

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -196,7 +196,7 @@ class App:
         )
         if filepath:
             self.array, img2show = read_dicom_file(filepath)
-            self.img1 = img2show.resize((250, 250), Image.ANTIALIAS)
+            self.img1 = img2show.resize((250, 250), Image.Resampling.LANCZOS)
             self.img1 = ImageTk.PhotoImage(self.img1)
             self.text_img1.image_create(END, image=self.img1)
             self.button1["state"] = "enabled"
