7094c0f3eb562ed912e0ae04db795fcab6c8feb7
Miguel Sanabria <miguel_ang.sanabria@uao.edu.co>
2024-07-21 23:39:20 -0500

    Add multi file support

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -201,7 +201,11 @@ class App:
             ),
         )
         if filepath:
-            self.array, img2show = read_dicom_file(filepath)
+            file_extension = filepath.split('.')[-1].lower()
+            if file_extension == 'dcm':
+                self.array, img2show = read_dicom_file(filepath)
+            elif file_extension in ['jpg', 'jpeg', 'png']:
+                self.array, img2show = read_jpg_file(filepath)
             self.img1 = img2show.resize((250, 250), Image.Resampling.LANCZOS)
             self.img1 = ImageTk.PhotoImage(self.img1)
             self.text_img1.image_create(END, image=self.img1)
